8a50a0d9e7f953477c3817fc3cf98246b2128490
Jairus <me@jairus.dev>
2025-03-10 11:57:09 -0700

    chore: add benchmarks back

---
+++ assembly/__benches__/abc.bench.ts
@@ -0,0 +1,21 @@
+import { JSON } from "..";
+import { bench } from "../custom/bench";
+
+const v1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const v2 = '"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"';
+
+bench(
+  "Serialize Alphabet",
+  () => {
+    JSON.stringify(v1);
+  },
+  64_000_00,
+);
+
+bench(
+  "Deserialize Alphabet",
+  () => {
+    JSON.parse<string>(v2);
+  },
+  64_000_00,
+);
--- bench/lib/schemas.js
+++
@@ -1,6 +0,0 @@
-export class Vec3 {
-    x;
-    y;
-    z;
-}
-//# sourceMappingURL=schemas.js.map

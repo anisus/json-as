5edc545112237ac417715b826f8126800532a08c
Accipiter Nisus <accipiter@gmail.com>
2025-06-16 11:08:22 +0200

    Added tests with deserialization and serialization of objects in namespaces. Updated run-test.sh to allow argument to run specific test.

--- run-tests.sh
+++ run-tests.sh
@@ -4,24 +4,28 @@ mkdir -p ./build
 
 for file in ./assembly/__tests__/*.spec.ts; do
   filename=$(basename -- "$file")
-  output="./build/${filename%.ts}.wasm"
+  if [ -z "$1" ] || [ "$1" = "$filename" ]; then
+    output="./build/${filename%.ts}.wasm"
 
-  start_time=$(date +%s%3N)
-  npx asc "$file" --transform ./transform -o "$output" --enable simd --config ./node_modules/@assemblyscript/wasi-shim/asconfig.json --disableWarning 226 || { echo "Tests failed"; exit 1; }
-  end_time=$(date +%s%3N) 
+    start_time=$(date +%s%3N)
+    npx asc "$file" --transform ./transform -o "$output" --enable simd --config ./node_modules/@assemblyscript/wasi-shim/asconfig.json --disableWarning 226 || { echo "Tests failed"; exit 1; }
+    end_time=$(date +%s%3N)
 
-  build_time=$((end_time - start_time))
+    build_time=$((end_time - start_time))
 
-  if [ "$build_time" -ge 60000 ]; then
-    formatted_time="$(bc <<< "scale=2; $build_time/60000")m"
-  elif [ "$build_time" -ge 1000 ]; then
-    formatted_time="$(bc <<< "scale=2; $build_time/1000")s"
+    if [ "$build_time" -ge 60000 ]; then
+      formatted_time="$(bc <<< "scale=2; $build_time/60000")m"
+    elif [ "$build_time" -ge 1000 ]; then
+      formatted_time="$(bc <<< "scale=2; $build_time/1000")s"
+    else
+      formatted_time="${build_time}ms"
+    fi
+
+    echo " -> $filename (built in $formatted_time)"
+    wasmtime "$output" || { echo "Tests failed"; exit 1; }
   else
-    formatted_time="${build_time}ms"
+    echo " -> $filename (skipped)"
   fi
-
-  echo " -> $filename (built in $formatted_time)"
-  wasmtime "$output" || { echo "Tests failed"; exit 1; }
 done
 
 echo "All tests passed"

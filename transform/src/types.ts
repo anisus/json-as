f9e640b23b1e3c311dbb7ca2d48219ed9e779717
Accipiter Nisus <accipiter@gmail.com>
2025-06-17 09:14:12 +0200

    GH-148: Added comments to Src methods. Removed unused JSONTransform import.

--- transform/src/types.ts
+++ transform/src/types.ts
@@ -13,7 +13,7 @@ import {
   CommonFlags,
 } from "assemblyscript/dist/assemblyscript.js";
 import { TypeAlias } from "./linkers/alias.js";
-import { JSONTransform, stripNull } from "./index.js";
+import { stripNull } from "./index.js";
 
 export enum PropertyFlags {
   OmitNull,
@@ -91,6 +91,12 @@ export class Schema {
 export class SourceSet {
   private sources: Record<string, Src> = {};
 
+  /**
+   * Get a stored source from the set, or create a new one and store it if it
+   * didn't exist.
+   * @param source AssemblyScript Source
+   * @returns Source object
+   */
   get(source: Source): Src {
     let src = this.sources[source.internalPath]
     if (!src) {
@@ -119,6 +125,11 @@ export class Src {
     this.traverse(source.statements, []);
   }
 
+  /**
+   * Traverse source nodes and finds all classes and imports, and which namespaces they exist under.
+   * @param nodes Nodes to traverse.
+   * @param path The current path of namespace declarations leading to the nodes.
+   */
   private traverse(nodes: Node[], path: NamespaceDeclaration[]) {
     for (let node of nodes) {
       switch (node.kind) {
@@ -139,14 +150,30 @@ export class Src {
     }
   }
 
+  /**
+   * Get the qualified name (eg. "Namespace.BaseObject") for a class.
+   * @param node Class declaration.
+   * @returns Qualified name
+   */
   getQualifiedName(node: DeclarationStatement): string {
     return this.qualifiedName(node, this.nodeMap.get(node));
   }
 
+  /**
+   * Get a class declaration by its qualified name.
+   * @param qualifiedName Qualified named (eg. "Namespace.BaseObject")
+   * @returns Class declaration or null if not found.
+   */
   getClass(qualifiedName: string): ClassDeclaration | null {
     return this.classes[qualifiedName] || null;
   }
 
+  /**
+   * Get imported class from other sources in the parser.
+   * @param qualifiedName Qualified name of class.
+   * @param parser AssemblyScript parser.
+   * @returns Class declaration or null if not found.
+   */
   getImportedClass(qualifiedName: string, parser: Parser): ClassDeclaration | null {
     for (const stmt of this.imports) {
       const externalSource = parser.sources
@@ -164,7 +191,7 @@ export class Src {
   }
 
   /**
-   * Returns a unique path string to the node by combining the internalPath with
+   * Gets a unique path string to the node by combining the internalPath with
    * the qualified name of the node.
    * @param node DeclarationStatement
    */
@@ -172,6 +199,12 @@ export class Src {
     return this.internalPath + '/' + this.getQualifiedName(node);
   }
 
+  /**
+   * Resolved the qualified name of the extended class for a class
+   * declaration.
+   * @param classDeclaration Class declaration that extends another class.
+   * @returns Qualified name of the extended class, or empty string if not extending any class.
+   */
   resolveExtendsName(classDeclaration: ClassDeclaration): string {
     const parents = this.nodeMap.get(classDeclaration);
     if (!classDeclaration.extendsType || !parents) {
@@ -196,12 +229,24 @@ export class Src {
     return extendsName;
   }
 
+  /**
+   * Get the qualified name (eg "Namespace.BaseObject") of a class.
+   * @param node Class declaration.
+   * @param parents Array of namespace parents.
+   * @returns Qualified name
+   */
   private qualifiedName(node: DeclarationStatement, parents: NamespaceDeclaration[]): string {
     return parents?.length
       ? parents.map((p) => p.name.text).join('.') + '.' + node.name.text
       : node.name.text;
   }
 
+  /**
+   * Checks if the node is either a namespace or class, and returns the simple
+   * name of the node.
+   * @param node Node to check
+   * @returns Name of namespace or class, or empty string if other type of node.
+   */
   private getNamespaceOrClassName(node: Node): string {
     switch (node.kind) {
       case NodeKind.NamespaceDeclaration:
@@ -212,6 +257,12 @@ export class Src {
     return "";
   }
 
+  /**
+   * Get the full name (eg. "Namespace.Base") of a type name such as an
+   * extendedType.
+   * @param typeName Type name
+   * @returns Full name
+   */
   private getIdentifier(typeName: TypeName): string {
     let names = [];
     while (typeName) {
